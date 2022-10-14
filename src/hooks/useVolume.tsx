61e383e9fec00c74527062d2567e8427e03e343a
Castle <castlerock88@protonmail.com>
2022-10-14 07:20:53 -0400

    feat: wow play on volume change

--- src/hooks/useVolume.tsx
+++ src/hooks/useVolume.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useCallback, useContext, useState } from "react";
+import useOwen from "../hooks/useOwen";
 
 const VolumeContext = createContext<
   | {
@@ -12,10 +13,20 @@ const storage = localStorage.getItem("volume");
 
 const VolumeProvider = ({ children }: { children: React.ReactNode }) => {
   const [volume, setVolume] = useState(storage ? +storage : 50);
-  const handleVolume = useCallback((vol: number) => {
-    setVolume(vol);
-    localStorage.setItem("volume", "" + vol);
-  }, []);
+  const { owen, randomWow, activeWow } = useOwen();
+  const handleVolume = useCallback(
+    (vol: number) => {
+      setVolume(vol);
+      const audio = new Audio(activeWow);
+      audio.volume = 0.01 * volume;
+      audio.play();
+      if (owen.data) {
+        randomWow(owen.data);
+      }
+      localStorage.setItem("volume", "" + vol);
+    },
+    [volume]
+  );
 
   const data = {
     volume,
